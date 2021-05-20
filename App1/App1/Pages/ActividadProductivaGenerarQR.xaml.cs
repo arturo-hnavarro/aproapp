7ec6b05cf605baca1f016db7b50c61776ef80264
arturohernandez-aeropost <arturo.hernandez@aeropost.com>
2021-05-19 21:35:52 -0600

    genera codigo QR

--- App1/App1/Pages/ActividadProductivaGenerarQR.xaml.cs
+++ App1/App1/Pages/ActividadProductivaGenerarQR.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using QRCoder;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,20 +15,36 @@ namespace Approagro.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ActividadProductivaGenerarQR : ContentPage
     {
-        private int Id;
+        
+        private readonly string QRStringValue = "APROAGRO|ACTIVIDAD|";
         public ActividadProductivaGenerarQR()
         {
             InitializeComponent();
         }
         public ActividadProductivaGenerarQR(int id)
         {
-            Id = id;
             InitializeComponent();
+            QR.BarcodeValue = QRStringValue + id;
+            //https://www.youtube.com/watch?v=Kkub8wWf6HA
         }
-        protected override async void OnAppearing()
+        /*protected override async void OnAppearing()
         {
             base.OnAppearing();
-            
+            /*
+             * 
+            QRCodeGenerator qRCode = new QRCodeGenerator();
+            QRCodeData data = qRCode.CreateQrCode(Id.ToString(), QRCodeGenerator.ECCLevel.Q);
+            QRCode newCode = new QRCode(data);
+
+            Bitmap bitmap = newCode.GetGraphic(5);
+            //            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "QRCode_temp.jpg");
+
+            newCode.GetGraphic(5)
+        }*/
+
+        async void OnExportarClick(object sender, EventArgs e)
+        {
+
         }
     }
 }
