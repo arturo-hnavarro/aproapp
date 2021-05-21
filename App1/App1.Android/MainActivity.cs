c23759f6b41d80104101b1e08352afe29e2fb8f6
arturohernandez-aeropost <arturo.hernandez@aeropost.com>
2021-05-21 14:06:58 -0600

    agrega metodos para generar QR y exportarlos para que los puedan imprimir

--- App1/App1.Android/MainActivity.cs
+++ App1/App1.Android/MainActivity.cs
@@ -20,14 +20,15 @@ namespace Approagro.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState); // add this line to your code, it may also be called: bundle
+//            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
 
             //to receive incoming notifications:
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
             CreateNotificationFromIntent(Intent);
+
 
             LoadApplication(new App());
         }
--- App1/App1/Pages/ActividadProductivaGenerarQR.xaml.cs
+++ App1/App1/Pages/ActividadProductivaGenerarQR.xaml.cs
@@ -1,13 +1,9 @@
 ﻿using Approagro.Domain;
-using QRCoder;
 using System;
-using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
+using ImageFromXamarinUI;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,8 +12,9 @@ namespace Approagro.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ActividadProductivaGenerarQR : ContentPage
     {
-        
+
         private readonly string QRStringValue = "APROAGRO|";
+        private ActividadProductiva actividad;
         public ActividadProductivaGenerarQR()
         {
             InitializeComponent();
@@ -25,27 +22,84 @@ namespace Approagro.Pages
         public ActividadProductivaGenerarQR(ActividadProductiva actividadProductiva)
         {
             InitializeComponent();
-            QR.BarcodeValue = $"{QRStringValue}{actividadProductiva.NombreActividad}|{actividadProductiva.IdActividad}";
-            //https://www.youtube.com/watch?v=Kkub8wWf6HA
+            actividad = actividadProductiva;
+            GenerateQR();
         }
-        /*protected override async void OnAppearing()
+
+        /// <summary>
+        /// Generate QR Code using ZXwing library from Nuget
+        /// https://www.youtube.com/watch?v=Kkub8wWf6HA
+        /// </summary>
+        private void GenerateQR()
         {
-            base.OnAppearing();
-            /*
-             * 
-            QRCodeGenerator qRCode = new QRCodeGenerator();
-            QRCodeData data = qRCode.CreateQrCode(Id.ToString(), QRCodeGenerator.ECCLevel.Q);
-            QRCode newCode = new QRCode(data);
-
-            Bitmap bitmap = newCode.GetGraphic(5);
-            //            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "QRCode_temp.jpg");
-
-            newCode.GetGraphic(5)
-        }*/
-
-        async void OnExportarClick(object sender, EventArgs e)
+            QR.BarcodeValue = $"{QRStringValue}{actividad.NombreActividad}|{actividad.IdActividad}";
+        }
+        void OnExportarClick(object sender, EventArgs e)
         {
+            try
+            {
+                CreateAndShareQR();
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ups, no fue posible realizar la operación", ex.Message, "Aceptar");
+            }
+        }
 
+
+        /// <summary>
+        /// Method from xamarin docs available on https://docs.microsoft.com/en-us/xamarin/essentials/get-started?tabs=windows%2Candroid
+        /// </summary>
+        /// <returns></returns>
+        async Task CaptureScreenshot()
+        {
+            var screenshot = await Screenshot.CaptureAsync();
+            var stream = await screenshot.OpenReadAsync();
+
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "QRCode_temp.jpg");
+            using (var fileStream = File.Create(path))
+            {
+                stream.Seek(0, SeekOrigin.Begin);
+                stream.CopyTo(fileStream);
+            }
+        }
+
+
+        /// <summary>
+        /// Based on the implementation of https://www.youtube.com/watch?v=O9D3NSYh1t0
+        /// uses ImageFromXamarinUI nuget
+        /// </summary>
+        async void PartialCaptureScreenshot()
+        {
+            var screenshotStream = await QR.CaptureImageAsync();
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "QRCode_temp.jpg");
+            using (var fileStream = File.Create(path))
+            {
+                screenshotStream.Seek(0, SeekOrigin.Begin);
+                screenshotStream.CopyTo(fileStream);
+            }
+        }
+
+        /// <summary>
+        /// Share the QR code generated
+        /// </summary>
+        private void ShareQR()
+        {
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "QRCode_temp.jpg");
+            //TODO: implementar logica
+        }
+
+        private void CreateAndShareQR()
+        {
+            try
+            {
+                PartialCaptureScreenshot();
+                ShareQR();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
